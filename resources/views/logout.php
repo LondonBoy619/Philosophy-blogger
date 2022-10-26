a7273d377bab8df02ed8627bc6fda6a2af862453
unknown <london619boy@gmail.com>
2022-10-26 14:23:27 +0300

    Removed unused views/routes

--- resources/views/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-include($_SERVER['DOCUMENT_ROOT'].'/includes/query_functions.php');
-	if(checkCookie('logged_in')){
-		setCookie('logged_in', 'yes', time(3600));
-	}
-	header('location: index.php');
-
-?>
